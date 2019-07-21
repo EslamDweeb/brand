064a1baeb705f1e78f3ecdac500919fab0bc6768
Eslam Dweeb <e.dweeb@solutionplus.net>
2019-07-21 11:05:59 +0200

    d

---
+++ Brand/Scens/ItemDetail/FirstCell/ItemDetailCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemDetailCell.swift
+//  Brand
+//
+//  Created by Eslam Dweeb on 7/21/19.
+//  Copyright © 2019 Eslam Dweeb. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailCell:UICollectionViewCell{
+    let detailView = DetailView()
+    let descriptionView = DescriptionView()
+    let footerView = FooterView()
+    
+    
+}
