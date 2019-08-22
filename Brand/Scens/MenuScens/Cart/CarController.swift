515a375c0d56aeab50d8fbc652a5d15a20c3c0d8
Yusef Naser <y.naser@gmail.com>
2019-08-22 17:06:19 +0200

    fix some bugs

--- Brand/Scens/MenuScens/Cart/CarController.swift
+++ Brand/Scens/MenuScens/Cart/CarController.swift
@@ -54,7 +54,7 @@ class CartController : UIViewController ,ButtonActionDelegate{
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-       // getCartData(true)
+        getCartData(true)
         stopNotifier(reachability: reachability)
     }
     //MARK:- Helper Functions
